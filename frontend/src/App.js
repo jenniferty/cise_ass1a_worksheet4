2db9c8c664eb3da24e4e3bde37980d956213e040
jenniferty <bqd3000@autuni.ac.nz>
2022-08-20 20:31:47 +1200

    add style sheets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Route, 
+  NavLink,
+  BrowserRouter as Router,
+  Redirect
+} from 'react-router-dom';
 
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article"; 
-//import NotFoundPage from "./pages/404";
-import { Route, NavLink, BrowserRouter as Router} from 'react-router-dom';
+import NotFoundPage from "./pages/404";
+
 
 const App = () =>  {
     return (
@@ -20,6 +25,8 @@ const App = () =>  {
             <Route exact path="/" component={Home}/>
             <Route  path="/SEPractice" component={SEPractice}/>
             <Route  path="/SubmitArticle" component={SubmitArticle}/>
+            <Route exact path="/404" component={NotFoundPage}/>
+            <Redirect to="/404" />
           </div>
         </div>
         </Router>
