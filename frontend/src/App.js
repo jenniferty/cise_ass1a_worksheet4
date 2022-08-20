bda7709f5e96c410ed203ba41b1fc8edd64e78ee
jenniferty <bqd3000@autuni.ac.nz>
2022-08-20 19:12:36 +1200

    add pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-//import Home from "./pages/Home";
-//import SEPractice from "./pages/SE-Practice";
-//import SubmitArticle from "./pages/Submit-Article"; 
+import Home from "./pages/Home";
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article"; 
 //import NotFoundPage from "./pages/404";
 import { Route, NavLink, BrowserRouter as Router} from 'react-router-dom';
 
@@ -12,12 +12,14 @@ const App = () =>  {
         <div>
           <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
             <ul className="header">
-                <li><a href = "/">Home</a></li>
-                <li><a href = "/SEPractice">Select the Practice</a></li>
-                <li><a href = "/SubmitArticle">Submit an Article</a></li>
+              <li><NavLink exact to = "/">Home</NavLink></li>
+              <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+              <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
             </ul>
           <div className="content">
-            
+            <Route exact path="/" component={Home}/>
+            <Route  path="/SEPractice" component={SEPractice}/>
+            <Route  path="/SubmitArticle" component={SubmitArticle}/>
           </div>
         </div>
         </Router>
